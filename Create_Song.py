5347ff9db44f43fc146a88b35c810845cb099185
Christopher Lindsay <42160806+clindsa1@users.noreply.github.com>
2018-12-19 12:20:03 -0500

    created Create_Song.py
    just a stub out for now

---
+++ Create_Song.py
@@ -0,0 +1,18 @@
+import sys
+
+
+#read args
+#1) file name
+#2) notes
+#3) delays
+
+#verify
+#split into list and check len? or count ','?
+
+#create file
+	#if file already exists, exit
+
+#save data
+
+
+#close file
